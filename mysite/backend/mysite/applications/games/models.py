61e58b0852f958bbea8d8f2f0aceebd4e9b341dc
Carlos Chitty <chitty.carlosj@gmail.com>
2016-11-07 13:14:48 -0500

    Fixed typo in games model

--- mysite/backend/mysite/applications/games/models.py
+++ mysite/backend/mysite/applications/games/models.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Game(models.model):  
+class Game(models.Model):  
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=750)
