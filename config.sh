6f888fc218b6b3927a12c5058a300d8fa0f1f265
Carlos Chitty <chitty.carlosj@gmail.com>
2016-11-07 19:33:43 -0500

    Vagrant config updated

--- config.sh
+++ config.sh
@@ -1,26 +1,23 @@
-# Dependencies for the Django Angular demo
-
 # NodeJS 4.4.7+
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get update
-sudo apt-get install -y nodejs
+sudo apt-get -qqy update
+sudo apt-get -qqy install nodejs
 # npm 2.15.8+
-sudo apt install npm
+sudo apt -qqy install npm
 # Webpack 1.13.x
 sudo npm i -g webpack
 # ESLint 2.13.1+ 
 sudo npm i -g eslint
 # Python 2.x
-sudo apt install python
+sudo apt -qqy install python
 # Pip 9.0+
-sudo apt install python-pip
+sudo apt -qqy install python-pip
 pip install --upgrade pip
 # Virtualenv
 pip install virtualenv
 
 # Activate virtualenv
+virtualenv mysite
 cd mysite
 source bin/activate
-
-# To do: pip install requirements
-
+pip install -r backend/requirements.txt
--- readme.md
+++ readme.md
@@ -1,4 +1,25 @@
 # Game
 
+Application that display information about different games.
+
+
 ## Install instructions
+
+The repository includes a Vagrant Virtual Machine with all the project's dependencies installed. To get and install vagrant refer to [Vagrant's official website](https://www.vagrantup.com)
+
+To start the virtual machine and connect to it run:
+
+    vagrant up
+    vagrant ssh
+
+Before running the app, make sure the environment variables requiered for the app are loaded in `mysite/dev.env` for development or `mysite/prod.env` for production.
+
+    DATABASE_URL
+    DEBUG
+    FRONT_END
+    SECRET_KEY
+
+Once in the virtual machine terminal navigate to `/vagrant/backend/mysite` and run the app:
+    
+    python manage.py
 
